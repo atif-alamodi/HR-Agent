54e9fd82e51997d8a7923954cadd6d70b9032769
Atif <atif@hr-agent.local>
2026-06-13 01:55:28 +0000

    حاسبة حتمية داخل اللوحة (نهاية الخدمة/التسوية/التأمينات) تحسب بالكود لا بالنموذج، منع رؤية صور وهمية على النموذج المجاني، تشديد منع اختلاق الأرقام، إصدار v10/SW v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hra-v4";
+const CACHE = "hra-v5";
 const SHELL = ["./", "./index.html", "./icon-192.png", "./icon-512.png", "./manifest.webmanifest"];
 self.addEventListener("install", (e) => {
   e.waitUntil(
